d0c4355fd42387b8b585a7829674ccea4a37d69e
Ikamari <SilverFire098@gmail.com>
2017-09-19 20:25:30 +0300

    New features

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
-// How do you?
+$(document).ready(function () {
+    $('#JSON_data').on('click', function (event) {
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('#DB_data').on('click', function (event) {
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+    $('#API_data').on('click', function (event) {
+        $('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+});
