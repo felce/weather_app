b2ee6a35de40dc230d143c747c6d2d0363abb85c
ksand <sergkol557@gmail.com>
2017-09-19 19:46:33 +0300

    some fixes

--- main.php
+++ main.php
@@ -1,1 +1,17 @@
 <?php
+require 'database.php';
+require 'json.php';
+require 'api.php';
+ if (isset ($_REQUEST['data'])) {
+ 	$data = $_REQUEST['data'];
+
+ 	if (data === 'database'){
+ 		// echo getDataBase();
+    } elseif (data === 'json'){
+	    // echo getDataJson();
+    } elseif (data === 'api'){
+	    // echo getDataApi();
+    } else {
+    	echo 'bad request';
+    }
+ }
