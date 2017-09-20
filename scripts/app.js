cfee5092ada8e04831e0f6fec01a965984f326ca
Ikamari <SilverFire098@gmail.com>
2017-09-20 19:58:29 +0300

    Some little changes

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,32 @@ $(document).ready(function () {
     $('#JSON_data').on('click', function (event) {
         $('.active').removeClass('active');
         $(this).addClass('active');
+        getData('json');
     });
     $('#DB_data').on('click', function (event) {
         $('.active').removeClass('active');
         $(this).addClass('active');
+        getData('database');
     });
     $('#API_data').on('click', function (event) {
         $('.active').removeClass('active');
         $(this).addClass('active');
+        getData('api');
     });
+
+    function getData(dataType) {
+        $.get('main.php', {
+            data: dataType
+        }, function (result) {
+            result = JSON.parse(result);
+
+            //TODO: Fill our page
+            /*
+            >Current temperature
+            >Weather (ICON) < font awesome (sunny, rainy, snowy, cloudy)
+            >Time < unistump
+            >Object with {[time], [temperature], [weather])}
+             */
+        })
+    }
 });
