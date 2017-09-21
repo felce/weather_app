5680635e26c73df4045946b97fcb1406261321b1
felce <zabara.alexey@gmail.com>
2017-09-21 12:58:49 +0300

    api.php ready

--- api.php
+++ api.php
@@ -1,14 +1,86 @@
+
 <?php
+
 function getDataApi() {
-  $currentData = 'http://dataservice.accuweather.com/currentconditions/v1/324291?apikey=%09JyLaY8I0RaAV0YYKYJy6vAgoeyFpVf1p&language=en-us&details=false';
-  $lastSixHoutslData = 'http://dataservice.accuweather.com/currentconditions/v1/324291/historical?apikey=%09JyLaY8I0RaAV0YYKYJy6vAgoeyFpVf1p&language=en-us&details=false';
+  $currentData = 'http://dataservice.accuweather.com/currentconditions/v1/324291?apikey=%09alYZfQLu3cjFWdW5RJjzmeEn6S7RvM3t&language=en-us&details=false';
+  $lastSixHoutslData = 'http://dataservice.accuweather.com/currentconditions/v1/324291/historical?apikey=%09alYZfQLu3cjFWdW5RJjzmeEn6S7RvM3t&language=en-us&details=false';
   $lastData = file_get_contents($lastSixHoutslData);
   $lastjson = json_decode($lastData, true);
   $current = file_get_contents($currentData);
   $currentjson = json_decode($current, true);
+  $result = array(
+      'cur_time' => $currentjson[0]['EpochTime'],
+      'cur_temp' => $currentjson[0]['Temperature']['Metric']['Value'],
+      'cur_weather' => selectOption($currentjson[0]['WeatherText'])
+  );
+  for ($i = 1; $i < 7; $i++) {
+      $result[$i]['time'] = $lastjson[$i - 1]['EpochTime'];
+      $result[$i]['temp'] = $lastjson[$i - 1]['Temperature']['Metric']['Value'];
+      $result[$i]['weather'] = selectOption($lastjson[$i - 1]['WeatherText']);
+  }
 
-  $postData = json_encode(array('curretntTime' => $currentjson[0]['EpochTime'], curre), JSON_FORCE_OBJECT);
+  $resultstring = json_encode($result);
+  return $resultstring;
+}
 
+function selectOption($weathetText){
 
-  return $currentjson[0]['EpochTime'];
+  $sun = array(
+      'Sunny',
+      'Clear'
+  );
+  $cloud = array(
+      'Mostly sunny',
+      'Partly sunny',
+      'Intermittent clouds',
+      'Hazy cunshine',
+      'Mostly cloudy',
+      'Cloudy',
+      'Dreary (Overcast)',
+      'Mostly clear',
+      'Partly cloudy',
+      'Intermittent clouds',
+      'Hazy moonlight',
+      'Mostly cloudy'
+  );
+  $rain = array(
+      'Showers',
+      'Mostly cloudy w/ Showers',
+      'Partly sunny w/ Showers',
+      'T-Storms',
+      'Mostly cloudy w/ T-Storms',
+      'Partly sunny w/ T-Storms',
+      'Rain',
+      'Partly cloudy w/ Showers',
+      'Mostly cloudy w/ Showers',
+      'Partly cloudy w/ T-Storms',
+      'Mostly cloudy w/ T-Storms',
+      'Sleet',
+      'Freezing rain'
+  );
+  $snow = array(
+      'Flurries',
+      'Mostly cloudy w/ Flurries',
+      'Partly Sunny w/ Flurries',
+      'Snow',
+      'Mostly cloudy w/ Snow',
+      'Rain and snow',
+      'Mostly cloudy w/ Flurries',
+      'Mostly cloudy w/ Snow'
+  );
+  if (in_array($weathetText, $sun)) {
+      return 'sunny';
+  }
+  elseif (in_array($weathetText, $cloud)) {
+      return 'cloudy';
+  }
+  elseif (in_array($weathetText, $rain)) {
+      return 'rainy';
+  }
+  elseif (in_array($weathetText, $snow)) {
+      return 'snowy';
+  }
+  else {
+      return $weathetText;
+  }
 }
