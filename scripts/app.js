bd19069964c32318ffeaa2186b0737923ab5da4b
ksand <sergkol557@gmail.com>
2017-09-26 20:11:57 +0300

    fixed

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
 	getData('json');
+
 	$('.js-sources').click(function () {
 		$('.active').removeClass('active');
 		var sourceButton = $(this);
@@ -9,9 +10,10 @@ $(document).ready(function () {
 	});
 
 	function getData(dataType) {
+
 		$.get('main.php?data=' + dataType, function (result) {
 			clearPage();
-			console.log(result);
+			pushUrl(dataType);
 			var weatherList = '';
 			for (var key in result) {
 				var weatherInfo = result[key];
@@ -72,4 +74,8 @@ function clearPage() {
 	$('#forecast').html('');
 	$('#current_temperature').html('');
 	$('#date').html('');
+}
+
+function pushUrl(queryType) {
+	history.pushState({name:queryType}, '', '?data='+queryType);
 }
